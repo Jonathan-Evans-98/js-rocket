51ce74f167983cd3010a45b564b09eb77f2a3da8
Jonathan-Evans-98 <64919384+Jonathan-Evans-98@users.noreply.github.com>
2020-05-15 11:47:50 -0400

    created java and css files with some css and java script

---
+++ explanations.js
@@ -0,0 +1,33 @@
+//Explanations
+
+var favColor='blue';
+var myArray=['Blue', 'yellow', 'orange'];
+
+var numOfArray=3;
+var hasGotArray= true;
+var richObject={
+	firstName: 'Jonathan',
+	lastName: 'Evans',
+	yearsAlive: 22,
+	isMale: true
+}
+
+richObject.isMale
+
+//Functions
+var whatIsMyFavColor = Function () {
+
+	return 'blue';
+}
+
+var doSomething = Function
+
+
+var number1 = 10;
+var number2 = 20;
+
+if (number1 > number2) {
+	alert('true!!!');
+} else {
+	alert('false!!!')
+};
